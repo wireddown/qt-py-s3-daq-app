f3f772caa39d1d82e4152199cca93d94ab461c7e
Down to the Wire <8404598+wireddown@users.noreply.github.com>
2024-10-30 22:58:48 -0700

    Initialize package, configure poetry, configure tools, add tests (#10)
    * Configure poetry to create venvs in the project folder
    
    * Run poetry init
    
    ```
    Package name [qt-py-s3-daq-app]:  qtpy_datalogger
    Version [0.1.0]:
    Description []:  Data acquisition application using Adafruit QT Py S3
    Author [Down to the Wire <8404598+wireddown@users.noreply.github.com>, n to skip]:
    License []:  Apache-2.0
    Compatible Python versions [^3.11]:
    
    Would you like to define your main dependencies interactively? (yes/no) [yes] no
    Would you like to define your development dependencies interactively? (yes/no) [yes] no
    ```
    
    * Run poetry add pytest --group dev
    
    * Run poetry add ruff --group dev
    
    * Run poetry add pyright --group dev
    
    * Add poe tasks for running the tests and analyzers
    
    * Configure pytest
    
    --showlocals: Show locals in tracebacks
    
    * Configure pyright
    
    venvPath: let pyright use this venv to find dependencies
    verboseOutput: helpful for debugging errors
    reportAttributeAccessIssue: some third party code is atypical
    exclude: do not scan these folders
    
    * Configure global ruff settings
    
    line-length: 120 characters
    extend-exclude: do not scan the legacy code
    
    * Enable extra analyzer rule sets
    
    Listed at https://docs.astral.sh/ruff/rules/
    
    * Allow analyzer rules to be disabled
    
    * Disable D203 COM812 ISC001 which conflict with the formatter
    
    Recommended by running ruff itself
    
    * Disable D211 and use one blank line before class rather than no blank line
    
    * Disable D212 and use multi-line summary on second line rather than first line
    
    * Disable D400 and allow first line docstrings to end in any character
    
    * Disable G004 and allow f-strings in log messages
    
    * Disable RET504 and allow returning a local value
    
    This is helpful for debugging workflows
    
    * Allow analyzer rules to be disabled only for test code
    
    * Disable INP001 and do not require an __init__.py file for tests
    
    * Disable S101 and allow asserts in test code
    
    * Create qtpy_datalogger package and add import test
    
    * Create entry point for running qtpy_datalogger as a module and add test
    
    * Define main entry point function
    
    Set the run name to "__main__" during the test to mimic a Python invocation
    
    * Let poetry install the main entry point as a command
    
    * Set lint help to "Run the analyzers for the package."

---
+++ src/qtpy_datalogger/__main__.py
@@ -0,0 +1,10 @@
+"""Entry point for qtpy_datalogger package."""
+
+
+def main() -> None:
+    """Entry point function for qtpy_datalogger."""
+    print(__file__)
+
+
+if __name__ == "__main__":
+    main()
