f6ee652ed517a2a0c5ff68387448eaa22cb834f5
Down to the Wire <8404598+wireddown@users.noreply.github.com>
2024-10-31 22:12:17 -0700

    Add cli with logging (#11)
    * Experiment with enabling attr access reporting from pyright
    
    * Add dependency report when running pyright
    
    * Add poe fix task
    
      * ruff check --fix
      * ruff format
    
    * Run poetry add click
    
    * Add click command line interface and example acceptance tests
    
      * new submodule `console`
      * when invoked as a Python module, run the command line interface
      * implement click command line interface
        * add typical options: help, quiet, verbose, version
      * use logging for print statements
      * update main.py acceptance test with new click behavior
        * when called by name only, the program prints the help and no longer prints its filename
        * click raises the Exit error after printing help, so use the raises context manager to handle that event
    
    * Add a color logging handler as new tracelog submodule

---
+++ tests/test_console.py
@@ -0,0 +1,51 @@
+"""Tests for the qtpy_datalogger command line interface."""
+
+import logging
+
+import click
+import pytest
+from click.testing import CliRunner
+
+from qtpy_datalogger import console
+
+
+def test_console():  # noqa: ANN201
+    """Does it invoke the base command group?"""
+    runner = CliRunner()
+    result = runner.invoke(console.cli)
+    assert result.exit_code == 0
+    assert "Show this message and exit." in result.output
+
+
+def test_subcommand():  # noqa: ANN201
+    """Does it invoke a subcommand?"""
+    command_was_invoked_message = "subcommand_name invoked"
+
+    @click.command("subcommand_name")
+    def subcommand_name() -> None:
+        print(command_was_invoked_message)
+
+    console.cli.add_command(subcommand_name)
+
+    runner = CliRunner()
+    result = runner.invoke(
+        console.cli,
+        args=["subcommand_name"],
+    )
+    assert result.exit_code == 0
+    assert result.stdout.strip() == command_was_invoked_message
+
+
+@pytest.mark.parametrize(
+    ("quiet", "verbose", "expected_log_level", "assert_message"),
+    [
+        (False, False, logging.INFO, "Default logging level should be 'logging.INFO'"),
+        (True, False, logging.ERROR, "Quiet logging level should be 'logging.ERROR'"),
+        (False, True, logging.DEBUG, "Verbose logging level should be 'logging.DEBUG'"),
+        (True, True, logging.ERROR, "--quiet should override --verbose"),
+    ],
+)
+def test_verbosity_truth_table(quiet, verbose, expected_log_level, assert_message):  # noqa: ANN001 ANN201
+    """Validate all combinations of --quiet and --verbose."""
+    log_level = console.get_logging_level(quiet, verbose)
+    assert log_level == expected_log_level, assert_message
--- tests/test_main.py
+++ tests/test_main.py
@@ -4,6 +4,8 @@ import importlib
 import pathlib
 import runpy
 
+import pytest
+
 this_file = pathlib.Path(__file__).resolve()
 
 
@@ -14,8 +16,7 @@ def test_import_as_module():  # noqa: ANN201
 
 def test_run_as_module(capsys):  # noqa: ANN001, ANN201
     """Can Python invoke the entry point?"""
-    runpy.run_module("qtpy_datalogger", run_name="__main__")
+    with pytest.raises(SystemExit):
+        runpy.run_module("qtpy_datalogger", run_name="__main__")
     captured = capsys.readouterr()
-    expected_file = this_file.parent.parent / "src" / "qtpy_datalogger" / "__main__.py"
-    printed_file = pathlib.Path(captured.out.strip())
-    assert printed_file == expected_file, "main did not print its file path"
+    assert "Show this message and exit" in captured.out, "main did not print the help message"
