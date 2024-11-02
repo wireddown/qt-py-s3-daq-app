c055cea90b65decf636412985372b263a1938f54
Down to the Wire <8404598+wireddown@users.noreply.github.com>
2024-11-01 19:59:23 -0700

    Add a context manager to allow passing arguments to runpy.run_module()

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,12 +1,24 @@
 """Tests for the qtpy_datalogger package entry points."""
 
 import importlib
-import pathlib
 import runpy
+import sys
+from contextlib import contextmanager
 
 import pytest
 
-this_file = pathlib.Path(__file__).resolve()
+
+@contextmanager
+def program_args(args: list[str] | None = None):  # noqa: ANN201
+    """Use the specified args as sys.argv[1:] during a with: statement."""
+    old_sys_args = sys.argv
+    try:
+        args = args if args else []
+        new_sys_args = [sys.argv[0], *args]
+        sys.argv = new_sys_args
+        yield
+    finally:
+        sys.argv = old_sys_args
 
 
 def test_import_as_module():  # noqa: ANN201
@@ -16,7 +28,10 @@ def test_import_as_module():  # noqa: ANN201
 
 def test_run_as_module(capsys):  # noqa: ANN001, ANN201
     """Can Python invoke the entry point?"""
-    with pytest.raises(SystemExit):
+    with (
+        pytest.raises(SystemExit),
+        program_args(),
+    ):
         runpy.run_module("qtpy_datalogger", run_name="__main__")
     captured = capsys.readouterr()
     assert "Show this message and exit" in captured.out, "main did not print the help message"
