17cc3bd888e404069377f06ad0667f30c393ffbc
Down to the Wire <8404598+wireddown@users.noreply.github.com>
2025-01-13 20:25:53 -0800

    Start tracing keyboard codes

--- src/qtpy_sensor_node/snsr/pysh/py_shell.py
+++ src/qtpy_sensor_node/snsr/pysh/py_shell.py
@@ -1,18 +1,73 @@
 # Reworked from https://github.com/adafruit/Adafruit_CircuitPython_Prompt_Toolkit
 
+ORD_NUL = 0x00
+ORD_FKEY_START = 0x01
+ORD_BACKSPACE = 0x08
+ORD_TAB = 0x09
+ORD_LF = 0x0A
+ORD_CR = 0x0D
+ORD_EOF = 0x1A
+ORD_ESC = 0x1B
+ORD_OPEN_BRACKET = 0x5B
+ORD_LOWER_B = 0x62
+ORD_TILDE = 0x7E
+ORD_DEL = 0x7F
 
-KEY_ESCAPE = b"\x1b"
-KEY_CR = b"\x0d"
-KEY_LF = b"\x0a"
+NOOP_ORDS = [
+    # 0x00
+    # 0x01
+    0x02,
+    0x03,
+    0x04,
+    0x05,
+    0x06,
+    0x07,
+    # 0x08
+    # 0x09
+    # 0x0A
+    0x0B,
+    0x0C,
+    # 0x0D
+    0x0E,
+    0x0F,
+    0x10,
+    0x11,
+    0x12,
+    0x13,
+    0x14,
+    0x15,
+    0x16,
+    0x17,
+    0x18,
+    0x19,
+    # 0x1A
+    # 0x1B
+    0x1C,
+    0x1D,
+    0x1E,
+    0x1F,
+]
 
-KEY_OPEN_BRACKET = b"["
-ORD_OPEN_BRACKET = ord(KEY_OPEN_BRACKET)
+CONTROL_PATTERN_NONE = 0
+CONTROL_PATTERN_MOVE_CURSOR_KEY = 1  # up, down, right, left, end, home: code 0x1B then '['  then the single-ord command: one of [ABCDFH]
+CONTROL_PATTERN_EDITOR_KEY = 2       # Ins Del PgUp PgDown             : code 0x1B then '['  then the single-ord command: one of [2356]                 then the close '~'
+CONTROL_PATTERN_LOWER_F_KEY = 3      # F1..F4                          : code 0x01 then 'bO' then the single-ord command: one of [PQRS]
+CONTROL_PATTERN_UPPER_F_KEY = 4      # F5..F12                         : code 0x01 then 'b[' then the   dual-ord command:        [1][5789] or [2][0123] then the close '~'
 
-KEY_DEL = b"\x7f"
-ORD_DEL = ord(KEY_DEL)
 
+_PREVIOUS_ORD = ORD_NUL
 
-_PREVIOUS_ORD = 0
+_PRINTABLE_FOR_NONPRINTABLE = {
+    ORD_NUL: "(0)",
+    ORD_FKEY_START: "(F)",
+    ORD_BACKSPACE: "BS",
+    ORD_TAB: "TAB",
+    ORD_LF: "LF",
+    ORD_CR: "CR",
+    ORD_EOF: "EOF",
+    ORD_ESC: "ESC",
+    ORD_DEL: "DEL",
+}
 
 def __unused(*args):
     pass
@@ -22,8 +77,8 @@ def is_printable(char_ord):
     return char_ord > 31 and char_ord < 127
 
 
-def debug_str(in_charbyte):
-    return in_charbyte if is_printable(ord(in_charbyte)) else "?"
+def debug_str(in_ordinal):
+    return chr(in_ordinal) if is_printable(in_ordinal) else _PRINTABLE_FOR_NONPRINTABLE.get(in_ordinal, "?")
 
 
 # plink only sends CR (like classic macOS)
@@ -36,54 +91,93 @@ def _prompt(message="", *, input_=None, output=None, history=None, debug=False):
     output.write(message.encode("utf-8"))
     key_codes = []
     control_codes = []
+    control_pattern = CONTROL_PATTERN_NONE
     break_loop = False
-    while (not key_codes or _PREVIOUS_ORD not in [ord(KEY_CR), ord(KEY_LF)]) and not break_loop:
+    while (not key_codes or _PREVIOUS_ORD not in [ORD_CR, ORD_LF]) and not break_loop:
         in_char = input_.read(1)
-        in_ord = in_char[0]
-        debug(f"* received {in_ord:4}d {debug_str(in_char):4} previous {_PREVIOUS_ORD:4}")
+        in_ord = ord(in_char)
+        debug(f"* received {in_ord:4}d {debug_str(in_ord):4} previous {debug_str(_PREVIOUS_ORD):4}")
 
         if control_codes:
-            debug(f"** entering control char sequence {control_codes}")
             control_codes.append(in_ord)
-            if len(control_codes) == 2:
-                if in_ord != ORD_OPEN_BRACKET:
-                    debug(f"*** exiting control char sequence unsupported control code {in_ord:4}")
+            control_command_length = len(control_codes)
+            debug(f"** entering control char sequence {control_codes}")
+            if control_command_length == 2:
+                if control_codes[0] == ORD_ESC and in_ord == ORD_OPEN_BRACKET:
+                    # begin escape control sequence
+                    debug(f"** detected ESC control char sequence {control_codes}")
+                    control_pattern = CONTROL_PATTERN_MOVE_CURSOR_KEY  # Assume until further reads show otherwise
+                elif control_codes[0] == ORD_FKEY_START and in_ord == ORD_LOWER_B:
+                    # begin f-key control sequence
+                    debug(f"** detected F-key control char sequence {control_codes}")
+                    control_pattern = CONTROL_PATTERN_LOWER_F_KEY  # Assume until further reads show otherwise
+                else:
+                    debug(f"*** exiting control char sequence unsupported control code {debug_str(in_ord):4}")
+                    control_codes.clear()
+            elif control_command_length == 3:
+                if control_pattern == CONTROL_PATTERN_MOVE_CURSOR_KEY:
+                    if (ord("0") <= in_ord <= ord("9")):
+                        control_pattern = CONTROL_PATTERN_EDITOR_KEY  # We read more and learned we're reading an editor command
+                        debug(f"*** processing editor control code {debug_str(in_ord):4}")
+                    else:
+                        debug(f"*** completed move-cursor control code {debug_str(in_ord):4}")
+                        control_pattern = CONTROL_PATTERN_NONE
+                        control_codes.clear()
+                elif control_pattern == CONTROL_PATTERN_LOWER_F_KEY:
+                    if in_ord == ORD_OPEN_BRACKET:
+                        control_pattern = CONTROL_PATTERN_UPPER_F_KEY  # We read more and learned we're reading an upper F-key command
+                        debug(f"** detected upper F-key control char sequence {control_codes}")
+                    else:
+                        debug(f"*** processing lower F-key control code {debug_str(in_ord):4}")
+            elif control_command_length == 4:
+                if control_pattern == CONTROL_PATTERN_EDITOR_KEY:
+                    if in_ord == ORD_TILDE:
+                        debug(f"*** completed processing editor control code {debug_str(in_ord):4}")
+                        control_pattern = CONTROL_PATTERN_NONE
+                        control_codes.clear()
+                elif control_pattern == CONTROL_PATTERN_LOWER_F_KEY:
+                    debug(f"*** completed processing lower F-key control code {debug_str(in_ord):4}")
+                    control_pattern = CONTROL_PATTERN_NONE
                     control_codes.clear()
             else:
-                if (ord("0") <= in_ord <= ord("9")):
-                    debug(f"*** skipping control char sequence non-numeral control code {in_ord:4} {debug_str(in_char):4}")
-                else:
-                    debug(f"*** processing control code {in_ord:4}")
+                if in_ord == ORD_TILDE:
+                    debug(f"*** completed processing upper F-key control code {debug_str(in_ord):4}")
+                    control_pattern = CONTROL_PATTERN_NONE
                     control_codes.clear()
+                else:
+                    debug(f"*** processing upper F-key control code {debug_str(in_ord):4}")
             continue
-        if in_char in [KEY_ESCAPE]:
+
+        if in_ord in [ORD_ESC, ORD_FKEY_START]:
             debug("** detected control char sequence")
             control_codes.append(in_ord)
             continue
-        if in_char == KEY_LF and _PREVIOUS_ORD == ord(KEY_CR):
+
+        if in_ord == ORD_LF and _PREVIOUS_ORD == ORD_CR:
             # Throw away the line feed from Windows
             debug("** completed windows EOL")
             continue
-        if in_char in [KEY_CR, KEY_LF]:
+
+        if in_ord in [ORD_CR, ORD_LF]:
             # Do not capture EOL characters
             # Filter for more
-            # - F-keys start with \x01 and are either 2 more chars like 'bOP' 5 more chars like 'b[15~'
             # - Other non-printables
             # But allow
             # - Backspace \x08
             # - TAB \x09
             # - Delete
             debug("** skipped EOL char")
-            pass
+        elif in_ord in NOOP_ORDS:
+            debug(f"** skipped non printable char {in_ord}")
         else:
-            debug(f"** accepted {in_char}")
+            debug(f"** accepted {debug_str(in_ord)}")
             key_codes.append(in_ord)
 
         if in_ord == ORD_DEL:
             key_codes.pop()  # Remove the DEL char
             key_codes.pop()  # Remove the preceding char
             output.write(b"\b\x1b[K")  # Update the line
-        elif in_char in [KEY_CR, KEY_LF]:
+        elif in_ord in [ORD_CR, ORD_LF]:
             # Do not echo EOL characters back to the user
             debug("** matched loop exit")
             break_loop = True
@@ -91,7 +185,7 @@ def _prompt(message="", *, input_=None, output=None, history=None, debug=False):
             output.write(in_char)
 
         _PREVIOUS_ORD = in_ord
-        debug(f"\n** loop conditions key_codes: {key_codes} previous_ord: {_PREVIOUS_ORD} break: {break_loop}")
+        debug(f"\n** loop conditions key_codes: {key_codes} previous_ord: {debug_str(_PREVIOUS_ORD)} break: {break_loop}")
     output.write(b"\n")
     debug("encoded", key_codes)
 
