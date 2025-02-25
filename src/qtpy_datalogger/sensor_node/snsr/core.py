bdac23673fb5ea6180e08eaacf1a2f870393c1bb
Down to the Wire <8404598+wireddown@users.noreply.github.com>
2025-02-24 22:42:39 -0800

    Add paint_uart_line() to core

--- src/qtpy_datalogger/sensor_node/snsr/core.py
+++ src/qtpy_datalogger/sensor_node/snsr/core.py
@@ -18,6 +18,21 @@ def read_one_uart_line() -> str:
     return line
 
 
+def paint_uart_line(line: str) -> None:
+    """Erase and redraw the line with terminal control codes."""
+    import usb_cdc
+
+    from snsr.pysh.py_shell import redraw_line
+
+    # Using sys.stdio for serial IO with host
+    serial = usb_cdc.console
+    if usb_cdc.data:
+        # Switching to usb_cdc.data for serial IO with host
+        serial = usb_cdc.data
+
+    redraw_line(line, out_stream=serial)
+
+
 def get_memory_info() -> tuple[str, str]:
     """Return a tuple of formatted strings with used and free memory."""
     import gc
