6cd74c64c500362a1a4f48e81709a90af7db0a6d
Down to the Wire <8404598+wireddown@users.noreply.github.com>
2025-03-11 20:52:04 -0700

    Add helper functions for creating and parsing the MQTT message classes

--- src/qtpy_datalogger/sensor_node/snsr/core.py
+++ src/qtpy_datalogger/sensor_node/snsr/core.py
@@ -40,3 +40,53 @@ def get_memory_info() -> tuple[str, str]:
     used_bytes = gc.mem_alloc()
     free_bytes = gc.mem_free()
     return f"{used_bytes / 1024:.3f} kB", f"{free_bytes / 1024:.3f} kB"
+
+
+def get_system_info() -> dict[str, str]:
+    """Return a dictionary that describes the node."""
+    return {}
+
+
+def get_notice_info() -> dict:
+    """Return a json representation of the notice.toml file."""
+    import json
+
+    notice_contents = []
+    with open("/snsr/notice.toml", "r") as notice_toml:
+        notice_contents = notice_toml.read().splitlines()
+    notice_info = {}
+    for line in notice_contents:
+        key_and_value = line.split("=")
+        key = key_and_value[0].strip()
+        value = key_and_value[1].strip().replace('"', "")
+        notice_info[key] = value
+    return notice_info
+
+
+def get_descriptor_payload():
+    import json
+
+    from snsr.node.classes import (
+        DescriptorInformation,
+        DescriptorPayload,
+        NoticeInformation,
+        SenderInformation,
+        StatusInformation,
+    )
+
+    notice_info = get_notice_info()
+    notice = NoticeInformation(**notice_info)
+    descriptor = DescriptorInformation(node_id="me", hardware_name="esp", system_name="cpy", python_implementation="9.2.1", ip_address="wifi", notice=notice)
+    status = StatusInformation(used_memory="xx kB", free_memory="yy kB", cpu_temperature="zz C")
+    sender = SenderInformation(descriptor_topic="qtpy/v1/....", sent_at="now", status=status)
+    response = DescriptorPayload(descriptor=descriptor, sender=sender)
+    return json.dumps(response.as_dict())
+
+
+def get_command_payload(message: str) -> dict[str, str]:
+    import json
+
+    from snsr.node.classes import ActionPayload
+
+    action_payload = ActionPayload.from_dict(json.loads(message))
+    return action_payload
