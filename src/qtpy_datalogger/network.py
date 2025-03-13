9272dd3e0631667bbe8970755615437fceacb9af
Down to the Wire <8404598+wireddown@users.noreply.github.com>
2025-03-12 22:10:22 -0700

    - Add class that attaches a context object to an MQTT client - Add class that control a QTPy mode using an MQTT client

--- src/qtpy_datalogger/network.py
+++ src/qtpy_datalogger/network.py
@@ -16,6 +16,61 @@ from .sensor_node.snsr.node import mqtt as node_mqtt
 logger = logging.getLogger(__name__)
 
 
+class MqttClientWithContext(gmqtt.Client):
+    """Wrap a gmqtt.Client with a context property."""
+
+    def __init__(self, client_id: str, context: object) -> None:
+        """
+        Create a gmqtt.Client and attach any custom object or class as a new property named context.
+
+        Use this context in client callbacks to access application specific information.
+        """
+        super().__init__(client_id)
+        self.context = context
+
+
+class QTPyController:
+    """Class for controlling QT Py nodes."""
+
+    def __init__(self) -> None:
+        """Return a QTPyController."""
+        self.mac_address = "MUXaddr"
+        self.pid = "0"
+        self.mqtt_client_id = f"host-{self.mac_address}-{self.pid}"
+
+        self.broker_host = "localhost"
+        self.group_id = "centrifuge"
+
+        self.data_queue = asyncio.Queue()
+
+        # Define these at runtime because
+        #   we cannot change their parameters and make them instance methods (ie we cannot add 'self')
+        #   we don't want to make make them static methods and share them across all instances
+        def on_mqtt_connect(client, flags, rc, properties) -> None:
+            """Handle connection to the MQTT broker."""
+            logger.debug(f"Connected with flags='{flags}' rc='{rc}' properties='{properties}'")
+
+        async def on_mqtt_message(client, topic, payload, qos, properties) -> None:
+            """Handle a message on topic with payload."""
+            payload_string = payload.decode("UTF-8")
+            logger.debug(f"Received '{payload_string}' on '{topic}' with qos='{qos}' properties='{properties}'")
+            await client.context.data_queue.put(payload_string)
+
+        def on_mqtt_disconnect(client, packet) -> None:
+            """Handle disconnection from the MQTT broker."""
+            logger.debug(f"Disconnected with packet='{packet}'")
+
+        def on_mqtt_subscribe(client, mid, qos, properties) -> None:
+            """Handle subscription from the MQTT broker."""
+            logger.debug(f"Subscribed with mid='{mid}' qos='{qos}' properties='{properties}'")
+
+        self.client = MqttClientWithContext(self.mqtt_client_id, self)
+        self.client.on_connect = on_mqtt_connect
+        self.client.on_message = on_mqtt_message
+        self.client.on_disconnect = on_mqtt_disconnect
+        self.client.on_subscribe = on_mqtt_subscribe
+
+
 @contextlib.contextmanager
 def suppress_unless_debug() -> Generator[None, Any, None]:
     """Suppress logger.info() messages unless logging has been set to DEBUG / --verbose."""
@@ -56,32 +111,8 @@ async def _query_nodes_from_mqtt() -> dict[str, dict[str, str]]:
     descriptor_topic = all_topics["descriptor"]
     all_descriptors_in_group_topic = node_mqtt.get_descriptor_topic(group_id, node_id="+")
 
-    data_queue = asyncio.Queue()
-
-    def on_connect(client, flags, rc, properties) -> None:
-        """Handle connection to the MQTT broker."""
-        logger.debug(f"Connected with flags='{flags}' rc='{rc}' properties='{properties}'")
-
-    async def on_message(client, topic, payload, qos, properties) -> None:
-        """Handle a message on topic with payload."""
-        payload_string = payload.decode("UTF-8")
-        logger.debug(f"Received '{payload_string}' on '{topic}' with qos='{qos}' properties='{properties}'")
-        await data_queue.put(payload_string)
-
-    def on_disconnect(client, packet) -> None:
-        """Handle disconnection from the MQTT broker."""
-        logger.debug(f"Disconnected with packet='{packet}'")
-
-    def on_subscribe(client, mid, qos, properties) -> None:
-        """Handle subscription from the MQTT broker."""
-        logger.debug(f"Subscribed with mid='{mid}' qos='{qos}' properties='{properties}'")
-
-    client = gmqtt.Client(client_id)
-
-    client.on_connect = on_connect
-    client.on_message = on_message
-    client.on_disconnect = on_disconnect
-    client.on_subscribe = on_subscribe
+    controller = QTPyController()
+    client = controller.client
 
     with suppress_unless_debug():
         await client.connect(broker_host)
@@ -161,8 +192,8 @@ async def _query_nodes_from_mqtt() -> dict[str, dict[str, str]]:
         await client.disconnect()
 
     discovered_sensor_nodes = {}
-    while not data_queue.empty():
-        response_json = await data_queue.get()
+    while not controller.data_queue.empty():
+        response_json = await controller.data_queue.get()
         try:
             response = json.loads(response_json)
             if "action" in response:
@@ -175,5 +206,5 @@ async def _query_nodes_from_mqtt() -> dict[str, dict[str, str]]:
         except json.JSONDecodeError:
             pass
         # discovered_sensor_nodes[response["node_identifier"]] = response["node_group"]
-        data_queue.task_done()
+        controller.data_queue.task_done()
     return discovered_sensor_nodes
