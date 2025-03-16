f7753acafbc2138a1473e89810e401103c9d5279
Down to the Wire <8404598+wireddown@users.noreply.github.com>
2025-03-15 19:22:40 -0700

    Add formatter for sensor_node MQTT client IDs

--- src/qtpy_datalogger/sensor_node/snsr/node/mqtt.py
+++ src/qtpy_datalogger/sensor_node/snsr/node/mqtt.py
@@ -34,3 +34,7 @@ def get_log_topic(group_id: str) -> str:
 
 def get_result_topic(group_id: str, node_id: str) -> str:
     return f"qtpy/v1/{group_id}/{node_id}/result"
+
+
+def format_mqtt_client_id(role: str, mac_address: str, pid: int) -> str:
+    return f"{role}-{mac_address}-{pid}"
