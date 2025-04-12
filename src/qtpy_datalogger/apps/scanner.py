1884adf48659dc4ffd75bd3ba2aeb08e1d23a616
Down to the Wire <8404598+wireddown@users.noreply.github.com>
2025-04-12 15:58:28 -0700

    Add app title label

--- src/qtpy_datalogger/apps/scanner.py
+++ src/qtpy_datalogger/apps/scanner.py
@@ -3,8 +3,14 @@
 import asyncio
 import logging
 import pathlib
+import tkinter as tk
+from tkinter import font
 
-import qtpy_datalogger.guikit as guikit
+import ttkbootstrap as ttk
+import ttkbootstrap.icons as ttk_icons
+from ttkbootstrap import constants as bootstyle
+
+from qtpy_datalogger import guikit
 
 logger = logging.getLogger(pathlib.Path(__file__).stem)
 
@@ -14,9 +20,23 @@ class ScannerApp(guikit.AsyncWindow):
 
     def create_user_interface(self) -> None:
         """Create the main window and connect event handlers."""
+        app_name = "QT Py Sensor Node Scanner"
+        self.root_window.minsize(width=600, height=800)
+        self.root_window.place_window_center()
+        self.root_window.title(app_name)
+        self.root = ttk.Frame(self.root_window)
+        icon = tk.PhotoImage(master=self.root_window, data=ttk_icons.Icon.question)
+        self.root_window.iconphoto(True, icon)
+
+        icon_emoji = ttk_icons.Emoji.get("telescope")
+        title_font = font.Font(weight="bold", size=24)
+        title_label = ttk.Label(self.root, font=title_font, text=f"{icon_emoji} {app_name}", padding=16)
+        title_label.grid()
+        self.root.pack()
 
     async def on_loop(self) -> None:
         """Update the UI with new information."""
+        await asyncio.sleep(0.1)
 
     def on_closing(self) -> None:
         """Clean up before exiting."""
