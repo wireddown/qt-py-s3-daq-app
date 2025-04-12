953fc3f2726c0f49042abb7d712d9afac44ae9f8
Down to the Wire <8404598+wireddown@users.noreply.github.com>
2025-04-12 10:39:37 -0700

    (fixup) Add explanation for using no title bar icon in AsyncWindow

--- src/qtpy_datalogger/guikit.py
+++ src/qtpy_datalogger/guikit.py
@@ -53,6 +53,7 @@ class AsyncWindow:
 
     def __init__(self) -> None:
         """Initialize a new Tk root and cache the asyncio event loop."""
+        # Let subclasses set the window icon
         self.root_window = ttk.Window(iconphoto=None)  # pyright: ignore reportArgumentType -- the type hint for library is incorrect
         self.io_loop = asyncio.get_running_loop()
 
