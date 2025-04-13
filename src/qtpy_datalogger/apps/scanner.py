f8314cfe4fda43362c001760c0a04c468146ff75
Down to the Wire <8404598+wireddown@users.noreply.github.com>
2025-04-13 14:20:21 -0700

    Remove custom styling

--- src/qtpy_datalogger/apps/scanner.py
+++ src/qtpy_datalogger/apps/scanner.py
@@ -23,28 +23,25 @@ class ScannerApp(guikit.AsyncWindow):
         """Create the main window and connect event handlers."""
         # Theme
         style = ttk.Style()
-        bootstyle_background = bootstyle.LIGHT
-        background = style.colors.get(bootstyle_background)
-        style.configure("TFrame", background=background)
+        style.theme_use("sandstone")
 
         # Window properties
         app_name = "QT Py Sensor Node Scanner"
         self.root_window.minsize(width=600, height=800)
-        self.root_window.place_window_center()
         self.root_window.title(app_name)
-        self.root = ttk.Frame(self.root_window, name="root_frame", padding=16)
         icon = tk.PhotoImage(master=self.root_window, data=ttk_icons.Icon.question)
         self.root_window.iconphoto(True, icon)
 
         # Title
+        self.root = ttk.Frame(self.root_window, name="root_frame", padding=16)
         icon_emoji = ttk_icons.Emoji.get("telescope")
         title_font = font.Font(weight="bold", size=24)
-        title_label = ttk.Label(self.root, font=title_font, text=f"{icon_emoji} {app_name}", padding=16, background=background)
+        title_label = ttk.Label(self.root, font=title_font, text=f"{icon_emoji} {app_name}", padding=16)
         title_label.pack()
 
         # Scan group
         scan_frame = ttk.Frame(self.root, name="scan_frame", padding=8)
-        group_input_label = ttk.Label(scan_frame, text="Group name", background=background)
+        group_input_label = ttk.Label(scan_frame, text="Group name")
         self.group_input = ttk.Entry(scan_frame)
         scan_button = ttk.Button(scan_frame, command=self.start_scan, text="Scan group")
         group_input_label.pack(side=tk.LEFT)
@@ -80,7 +77,7 @@ class ScannerApp(guikit.AsyncWindow):
         # App commands and status
         exit_button = ttk.Button(self.root, text="Quit", style=bootstyle.DANGER, command=self.exit)
         help_button = ttk.Button(self.root, text="Online help", style=bootstyle.OUTLINE, command=self.launch_help)
-        self.status_message = ttk.Label(self.root, text="OK", style=bootstyle.SUCCESS, background=background)
+        self.status_message = ttk.Label(self.root, text="OK", style=bootstyle.SUCCESS)
         exit_button.pack(side=tk.RIGHT, padx=8, pady=(8, 0))
         help_button.pack(side=tk.RIGHT, padx=(8, 0), pady=(8, 0))
         self.status_message.pack(side=tk.RIGHT, padx=8, pady=(8, 0))
